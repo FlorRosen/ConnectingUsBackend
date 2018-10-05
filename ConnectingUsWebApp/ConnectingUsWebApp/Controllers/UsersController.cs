9357cd6093cff4b592f3d864028b7effc1b1f94e
Flor Rosental <Flopirosental@hotmail.com>
2018-10-04 21:09:24 -0300

    Cambios en el User Controller con metedos de GetUsers y GetUser

--- ConnectingUsWebApp/ConnectingUsWebApp/Controllers/UsersController.cs
+++ ConnectingUsWebApp/ConnectingUsWebApp/Controllers/UsersController.cs
@@ -12,18 +12,16 @@ namespace ConnectingUsWebApp.Controllers
 {
     public class UsersController : ApiController
     {
+        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["AzureConnection"].ConnectionString;
         List<User> users = new List<User>();
 
-        public IEnumerable<User> GetAllUsers()
+        public IEnumerable<User> GetUsers()
         {
-
-            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["AzureConnection"].ConnectionString;
-
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
 
                 connection.Open();
-                String query = "select * from dbo.users";
+                String query = "select * from users";
 
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
@@ -32,29 +30,58 @@ namespace ConnectingUsWebApp.Controllers
                     {
                         while (reader.Read())
                         {
-                            User user = new User();
-                            user.Id = Int32.Parse(reader["id_user"].ToString());
-                            user.FirstName = reader["first_name"].ToString();
-                            user.LastName = reader["last_name"].ToString();
+                            User user = mapUserFromDB(reader);
 
                             users.Add(user);
-                          
+
                         }
                     }
                 }
             }
-
             return users;
         }
 
         public IHttpActionResult GetUser(int id)
         {
-            var User = users.FirstOrDefault((p) => p.Id == id);
-            if (User == null)
+            var user = new User();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                String query = "select * from users where id_user = " + id;
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            user = mapUserFromDB(reader);
+                        }
+                    }
+                }
+            }
+
+            if (user == null)
             {
                 return NotFound();
             }
-            return Ok(User);
+            return Ok(user);
+        }
+
+        private User mapUserFromDB(SqlDataReader reader)
+        {
+            User user = new User();
+            user.Id = Int32.Parse(reader["id_user"].ToString());
+            user.FirstName = reader["first_name"].ToString();
+            user.LastName = reader["last_name"].ToString();
+            user.Gender = reader["gender"].ToString();
+            user.PhoneNumber = reader["phone_number"].ToString();
+            user.CreateDate = Convert.ToDateTime(reader["create_date"].ToString());
+            user.DateOfBirth = Convert.ToDateTime(reader["birth_date"].ToString());
+
+            return user;
         }
     }
 }
