22a9a90fbaf706837610c38095ef02538f8d536f
Flor <>
2018-10-07 20:33:40 -0300

    Agregue el AddUser que agrega un user, junto a su respectiva account

--- ConnectingUsWebApp/ConnectingUsWebApp/Controllers/UsersController.cs
+++ ConnectingUsWebApp/ConnectingUsWebApp/Controllers/UsersController.cs
@@ -3,65 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web.Http;
 using ConnectingUsWebApp.Models;
 using System.Data.SqlClient;
 using System.Text;
+using System.Web.Http;
+using ConnectingUsWebApp.Repositories;
 
 namespace ConnectingUsWebApp.Controllers
 {
     public class UsersController : ApiController
     {
-        string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["AzureConnection"].ConnectionString;
-        List<User> users = new List<User>();
+        static UsersRepository usersRepo = new UsersRepository();
 
+        //Public Methods
         public IEnumerable<User> GetUsers()
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-
-                connection.Open();
-                String query = "select * from users";
-
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            User user = mapUserFromDB(reader);
-
-                            users.Add(user);
-
-                        }
-                    }
-                }
-            }
+            List<User> users = usersRepo.GetUsers();
             return users;
         }
 
         public IHttpActionResult GetUser(int id)
         {
-            var user = new User();
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-                String query = "select * from users where id_user = " + id;
-
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            user = mapUserFromDB(reader);
-                        }
-                    }
-                }
-            }
+            var user = usersRepo.GetUser(id);
 
             if (user == null)
             {
@@ -70,18 +33,22 @@ namespace ConnectingUsWebApp.Controllers
             return Ok(user);
         }
 
-        private User mapUserFromDB(SqlDataReader reader)
+        //POST api/users
+       //[Route("~/api/Users/AddUser")]
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] User user)
         {
-            User user = new User();
-            user.Id = Int32.Parse(reader["id_user"].ToString());
-            user.FirstName = reader["first_name"].ToString();
-            user.LastName = reader["last_name"].ToString();
-            user.Gender = reader["gender"].ToString();
-            user.PhoneNumber = reader["phone_number"].ToString();
-            user.CreateDate = Convert.ToDateTime(reader["create_date"].ToString());
-            user.DateOfBirth = Convert.ToDateTime(reader["birth_date"].ToString());
+            var ok = usersRepo.AddUser(user);
 
-            return user;
+            if (ok)
+            {
+                return NotFound();
+            }
+
+            return Ok(ok );
         }
+
+
+
     }
 }
